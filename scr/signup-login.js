1d3adf03a039eb44bd2f1eb5d6a4883b29bd57ab
ayush <adikumar0069@gmail.com>
2026-05-07 20:02:19 +0545

    added the validetion feature for email and passeord

--- scr/signup-login.js
+++ scr/signup-login.js
@@ -1,21 +1,68 @@
 import axios from "axios";
+import isEmail from "validator/lib/isEmail";
+
+
+
+function debounce(fnc, event_listner_target){
+
+    let timeout;
+
+    event_listner_target.addEventListener("input", ()=>{
+    clearTimeout(timeout);
+    timeout = setTimeout(()=>{
+               fnc();
+              }, 2000)
+});
+}
+
+const user_email_input = document.getElementById('email');
+const user_phone_no_input = document.getElementById('phone-no');
+const user_username_input = document.getElementById('username');
+const user_pass_input = document.getElementById('user-pass');
 
 const signup_btn = document.getElementById('signup-btn');
 const login_btn = document.getElementById('login-btn');
 
 
+
+let valid_user_email = "";
+let valid_user_pass = "";
+
+
+function validate_email(){
+    let user_email = user_email_input.value;
+    if (isEmail(user_email)) {
+        document.getElementById('email_feedback').textContent = ""
+        return valid_user_email = user_email;
+    } else {
+        document.getElementById('email_feedback').textContent = "Please provide a valid email"
+    }
+}
+
+function validate_pass(){
+    let user_pass = user_pass_input.value;
+    let checkpass = user_pass.length >= 8 &&
+                    /[A-Z]/.test(user_pass) &&
+                    /[a-z]/.test(user_pass) &&
+                    /[0-9]/.test(user_pass) &&
+                    /[^A-Za-z0-9]/.test(user_pass);
+    if(checkpass){
+        document.getElementById('pass_feedback').textContent = "";
+        return valid_user_pass = user_pass
+    } else{
+        document.getElementById('pass_feedback').textContent = "Please create a Strong password";
+    }
+}
+
+
 async function sendcred() {
     try {
-        let user_email = document.getElementById('email').value;
-        let user_phone_no = document.getElementById('phone-no').value;
-        let user_username = document.getElementById('username').value;
-        let user_pass = document.getElementById('user-pass').value;
         const response = await
-        axios.post('http://localhost:3000/api/userdata', {
-            user_email,
-            user_phone_no,
-            user_username,
-            user_pass
+        axios.post('http://10.47.43.103:3000/api/userdata', {
+            user_email: valid_user_email,
+            user_phone_no: user_phone_no,
+            user_username: user_username,
+            user_pass: valid_user_pass
         })
     }
     catch(err){
@@ -23,4 +70,7 @@ async function sendcred() {
     }
 }
 
-signup_btn.addEventListener("click", sendcred)
+signup_btn.addEventListener("click", sendcred);
+
+debounce(validate_email, user_email_input);
+debounce(validate_pass, user_pass_input);
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const bcrypt = require("bcrypt");
+const pool = require("./dbconfig");
 const app = express();
+
 
 app.use(express.static('Static'));
 app.use(express.json());
+
 
 app.get('/', (req, res)=>{
     let auth = false;
@@ -18,12 +22,35 @@ app.get('/signup', (req,res)=>{
     res.sendFile(path.join(__dirname,'./Public', 'signup.html'));
 })
 
-app.post("/api/userdata", (req,res)=>{
-    let { user_email, user_phone_no, user_username, user_pass } = req.body;
-    res.json({massage: "data resived"})
-    console.log(user_email, user_phone_no, user_username, user_pass);
+app.post("/api/userdata", async (req,res)=>{
+      let user_email = req.body.user_email;
+      let user_phone_no = req.body.user_phone_no;
+      let user_username = req.body.user_username;
+      let user_pass = req.body.user_pass;
+
+      let hash_pass = await bcrypt.hash(user_pass, 10);
+
+      insertdb();
+       
+      async function insertdb(){
+        try {
+            await pool.query("INSERT INTO user_info(email, phono_no, username, hash_pass) VALUES($1,$2,$3,$4)",
+            [user_email, user_phone_no, user_username, hash_pass]
+        )
+         res.json({massage: "data resived"});
+        } catch (error) {
+            if (error.code === '23505') {
+                console.log(error.constraint)      
+            }
+        }
+      }
 })
 
-app.listen('3000', ()=>{
+app.listen('3000', "0.0.0.0", ()=>{
     console.log("server working on port 3000");
 })
+  
+async function auth() {
+    
+}
+
